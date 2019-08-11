f98be2f7534af1908c0a7aa1d3faac745dddeec7
Chris <cboddy1@hotmail.com>
2019-08-11 22:19:55 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config()
+
+var querystring = require("querystring");
+var express = require("express"); // Express web server framework
+var request = require("request"); // "Request" library
+
+const TOKEN = process.env.KEY;
+const openLDBWS = require('ldbws-json');
+const operation = require('ldbws-json/LDBWSOperation');
+const api = new openLDBWS(TOKEN);
+
+const app = express();
+
+app.get("/", function(req,res) {
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.end("<h1>Hello world</h1>");
+});
+
+app.get("/departures", function(req,res) {
+	res.writeHead(200, {"Content-Type": "text/html"});
+	const station = req.query.station;
+	console.log(station);
+	const options = {"crs": station}
+	api.call(operation.GET_DEPARTURE_BOARD, options).then((board) =>{
+		const r = board.GetStationBoardResult;
+		const services = r.trainServices.service;
+		console.log(services);
+
+		for (s of services) {
+		res.write(`${s.std} ${s.destination.location.locationName} ${s.etd} ${s.platform}`);
+			res.write("<br/>");
+		};
+	});
+});
+
+port = process.env.PORT;
+console.log("Listening on",port);
+app.listen(port);
