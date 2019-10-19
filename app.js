ffd0bd445ad0e1dc717de0bc1560d0a7affbc926
Chris <cboddy1@hotmail.com>
2019-10-19 15:41:39 +0100

    SSE updating

--- app.js
+++ app.js
@@ -10,29 +10,46 @@ const operation = require('ldbws-json/LDBWSOperation');
 const api = new openLDBWS(TOKEN);
 
 const app = express();
+app.use(express.static('public'))
 
 app.get("/", function(req,res) {
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.end("<h1>Hello world</h1>");
+	res.end("<h1>Go to /departures</h1>");
 });
 
-app.get("/departures", function(req,res) {
-	res.writeHead(200, {"Content-Type": "text/html"});
+app.get("/departure_stream", function(req,res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive'
+	})
 	const station = req.query.station;
-	const n_services = req.query.top || 3;
+	if (station == null) {
+		res.write("data: no station specified\n\n");
+		res.end();
+		return
+	}
+	const n_services = req.query.top;
 	console.log(station);
+	depboard(res,station,n_services);
+});
+
+function depboard(res, station,n_services) {
 	const options = {"crs": station}
 	api.call(operation.GET_DEPARTURE_BOARD, options).then((board) =>{
 		const r = board.GetStationBoardResult;
-		const services = r.trainServices.service.slice(0,n_services);
-		console.log(services);
+		const services = r.trainServices.service;
+		if (n_services != null) {
+			services = services.slice(0,n_services)
+		}
+		var b = `<br/>`;
 		for (s of services) {
-			res.write(`${s.std} ${s.destination.location.locationName} ${s.etd} ${s.platform}`);
-			res.write("<br/>");
+			b = b + `${s.std} ${s.destination.location.locationName} ${s.etd} ${s.platform} <br/>`;
 		};
-		res.end("");
+		res.write("data: "+b+"\n\n");
+		setTimeout(() => depboard(res, station,n_services), 10*1000);
 	});
-});
+}
 
 port = process.env.PORT;
 console.log("Listening on",port);
